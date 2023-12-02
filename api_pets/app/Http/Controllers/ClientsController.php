4412622fb32631b7bc6b921c5713dfcbf35267cb
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-02 00:08:14 -0500

    feat:atualizar client

--- api_pets/app/Http/Controllers/ClientsController.php
+++ api_pets/app/Http/Controllers/ClientsController.php
@@ -76,7 +76,32 @@ class ClientsController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try{
+            $validator =[
+                'name'=>'string|min:3',
+                'cpf'=> 'string| min:11',
+                'contact'=>'string| min:11'
+            ];
+
+            $request->validate($validator);
+
+            $data = ClientModel::with(['people'])->find($id);
+            
+            if(empty($data)){
+            return $this->response("Cliente nao encontrado",null,false, Response::HTTP_NOT_FOUND);
+            }
+
+            $people = PeopleModel::find($data->people_id);
+            $people->update($request->all());
+
+            $data = ClientModel::with(['people'])->find($id);
+
+            return $this->response("Cliente".$data->people->name."atualizado com sucesso",$data);
+
+            }catch(\Exception $e)
+            {
+            return $this->response($e->getMessage(),null, false, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**

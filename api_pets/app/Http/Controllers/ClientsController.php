f43e38bfe05537cebd32d2817058b8948f4d31f6
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-01 23:49:59 -0500

    feat: metodo para crear un novo cliente

--- api_pets/app/Http/Controllers/ClientsController.php
+++ api_pets/app/Http/Controllers/ClientsController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client as ClientModel;
+use App\Models\People as PeopleModel;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ClientsController extends Controller
 {
@@ -11,7 +14,14 @@ class ClientsController extends Controller
      */
     public function index()
     {
-        //
+        try{
+            $data = ClientModel::with(['people'])->get();
+            $message = $data->count().$data->count()=== 1 ? 'cliente encontrado': 'clientes encontrados'."com sucesso.";
+            return $this->response($message,$data);
+
+        }catch(\Exception $e){
+            return $this->response($e->getMessage(),null, false, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -19,7 +29,26 @@ class ClientsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try{
+            $validator =[
+                'name'=>'requiered|string|min:3',
+                'cpf'=> 'string| min:11',
+                'contact'=>'string| min:11'
+            ];
+
+            $request->People::validate($validator);
+
+            $peoplePayload = empty($request->input('contact'))
+            ?$request->only('name','cpf')
+            :$request->all();
+
+            $people = PeopleModel::firstOrCreate('peoplePayLoad');
+            $data = ClientModel::create(['people_id'=> $people->id]);
+            return $this->response("Cliente".$data->people->name." cadastro com sucesso",$data);
+               
+            }catch(\Exception $e){
+            return $this->response($e->getMessage(),null, false, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -27,7 +56,19 @@ class ClientsController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try{
+            $data = ClientModel::with(['people'])->find($id);
+
+            if(empty($data)){
+                return $this->response('Cliente nao enontrado',null,false, Response::HTTP_NOT_FOUND);
+            }
+
+            $message = "Cliente".$data->people->name."encontrado com sucesso.";
+            return $this->response($message,$data);
+
+        }catch(\Exception $e){
+            return $this->response($e->getMessage(),null, false, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -43,6 +84,20 @@ class ClientsController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try{
+            $data = ClientModel::with(['people'])->find($id);
+
+            if(empty($data)){
+                return $this->response('Cliente nao enontrado',null,false, Response::HTTP_NOT_FOUND);
+            }
+
+            ClientModel::destroy($data->id);
+
+            $message = "Cliente".$data->people->name."deletado com sucesso.";
+            return $this->response($message,$data);
+
+        }catch(\Exception $e){
+            return $this->response($e->getMessage(),null, false, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }

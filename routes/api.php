47561ddee3a1ce78ade0028c0479496aa8d82763
Caroline Sampaio <caroline_08022@hotmail.com>
2023-12-03 23:48:02 -0300

    feat: adiciona rotas da aplicação

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PetController;
 use App\Http\Controllers\PeoplesController;
+use App\Http\Controllers\RaceController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('peoples', PeoplesController::class)->only(
@@ -11,3 +12,8 @@ Route::resource('peoples', PeoplesController::class)->only(
 Route::resource('pets', PetController::class)->only(
     ['index', 'show', 'store', 'update', 'destroy']
 );
+
+Route::get('races', [RaceController::class, 'index']);
+Route::post('races', [RaceController::class, 'store']);
+Route::put('races/{id}', [RaceController::class, 'update']);
+Route::delete('races/{id}', [RaceController::class, 'destroy']);

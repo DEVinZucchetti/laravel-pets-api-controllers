94880eea80292c66dd3b6f5f6affee96b01a8e80
Caroline Sampaio <caroline_08022@hotmail.com>
2023-12-04 00:26:16 -0300

    feat: adiciona metodo para listar raças

--- app/Http/Controllers/RaceController.php
+++ app/Http/Controllers/RaceController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Race;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Test\Constraint\ResponseStatusCodeSame;
 
 class RaceController extends Controller {
     public function index() {
+        try {
+            $races = Race::all();
+            return $this->response("Requisição realizada com sucesso.", $races);
+        } catch (\Exception $e) {
+            return $this->response($e->getMessage(), null, false, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public function store() {

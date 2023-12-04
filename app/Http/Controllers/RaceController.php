8247698f42ea8c343ead356cdbd19f834cc901c7
Caroline Sampaio <caroline_08022@hotmail.com>
2023-12-03 23:47:45 -0300

    feat: adiciona base model e controller

---
+++ app/Http/Controllers/RaceController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RaceController extends Controller {
+    public function index() {
+    }
+
+    public function store() {
+    }
+
+    public function show() {
+    }
+
+    public function update() {
+    }
+
+    public function destroy() {
+    }
+}
